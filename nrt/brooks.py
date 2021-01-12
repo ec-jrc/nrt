ab7c57e8384d7d47eb698df95173ab4674ae8b6f
Jonas Viehweger <jonasvieh@gmail.com>
2021-01-12 14:17:14 +0100

    working

--- nrt/brooks.py
+++ nrt/brooks.py
@@ -22,14 +22,13 @@ class Brooks(BaseNrt):
         self.set_xy(dataarray)
         X = self.build_design_matrix(dataarray, trend=self.trend,
                                      harmonic_order=self.harmonic_order)
-        beta, residuals = self._fit(X, dataarray=dataarray, reg=reg,
+        beta_full, residuals_full = self._fit(X, dataarray=dataarray, reg=reg,
                                     check_stability=check_stability,
                                     **kwargs)
-        self.beta = beta
 
         # Shewhart chart to get rid of outliers (clouds etc)
-        sigma = np.nanstd(residuals, axis=0)
-        shewhart_mask = np.abs(residuals) > (self.threshold * sigma)
+        sigma = np.nanstd(residuals_full, axis=0)
+        shewhart_mask = np.abs(residuals_full) > (self.threshold * sigma)
         masked_outliers = dataarray.copy()
         masked_outliers.values = np.where(shewhart_mask, np.nan, dataarray)
 
@@ -39,19 +38,8 @@ class Brooks(BaseNrt):
                                     **kwargs)
         self.beta = beta
 
-        # calculate residuals including outliers and get standard deviation to get accurate base values
-        # for monitoring
-        # TODO is it possible to not have to predict for everything but only for the masked values
-        #  which aren't in `residuals`?
-        beta_flat = self.beta.reshape(X.shape[1], -1)
-        resid = (np.dot(X, beta_flat) - dataarray.values.reshape(X.shape[0], -1)) \
-            .reshape(residuals.shape)
-        sigma = np.nanstd(resid, axis=0)
-
-        # screen outliers for the last time and calculate sigma to make monitoring more sensitive
-        shewhart_mask = np.abs(resid) > self.threshold * sigma
-        masked_outliers = np.where(shewhart_mask, np.nan, dataarray)
-        self.sigma = np.nanstd(masked_outliers, axis=0)
+        # get new standard deviation
+        self.sigma = np.nanstd(residuals, axis=0)
 
         # calculate EWMA control limits and save them
         # since control limits quickly approach a limit they are assumed to be stable after the training period
@@ -59,14 +47,16 @@ class Brooks(BaseNrt):
         self.cl_ewma = self.threshold * self.sigma * np.sqrt((self.sensitivity / (2 - self.sensitivity)))
 
         # calculate the EWMA value for the end of the training period and save it
-        self.ewma = self.calc_ewma(masked_outliers)[-1]
+        self.ewma = self.calc_ewma(residuals)[-1]
 
     def monitor(self, array, date):
         y_pred = self.predict(date)
         residuals = array - y_pred
 
         # Filtering of values with high threshold X-Bar and calculating new EWMA values
-        self.ewma = np.where(np.abs(residuals) > self.threshold * self.sigma,
+        residuals[np.abs(residuals) > self.threshold * self.sigma] = np.nan
+
+        self.ewma = np.where(np.isnan(residuals),
             self.ewma,
             (1 - self.sensitivity) * self.ewma + self.sensitivity * residuals)
 
