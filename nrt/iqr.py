3dcc8d389e32ee210fd8b0583500011aa191c197
Loïc Dutrieux <loic.dutrieux@gmail.com>
2021-01-29 18:26:37 +0100

    Revert "Trying to integrate forest/non-forest mask with monitoring"
    This reverts commit a488571b3a9c5f24c05b54ba6d9a365aebb4378d.

--- nrt/iqr.py
+++ nrt/iqr.py
@@ -32,50 +32,26 @@ class Iqr(BaseNrt):
         self.q75 = q75
 
     def monitor(self, array, date):
-        """Run online monitoring on a new acquisition
-
-        Args:
-            array (numpy.ndarray): A 2D numpy array containing the values of the
-                variable being monitored. Areas of no data or invalid data must me
-                marked with ``np.nan``
-            date (datetime.datetime): The date of the acquisition
-
-        Returns:
-            The method is used for its side effect of updating the ``state`` and
-            ``mask`` slots of the class instance.
-        """
         iqr = self.q75 - self.q25
         lower_limit = self.q25 - self.sensitivity * iqr
         upper_limit = self.q75 + self.sensitivity * iqr
         y_pred = self.predict(date)
-        # Mask out non forest, or already detected breaks and compute residuals
-        array = np.where(self.mask == 1, array, np.nan)
-        is_monitored = np.isfinite(array)
+        # Compute residuals
         residuals = array - y_pred
         # compare residuals to threshold
         is_outlier = np.logical_or(residuals > upper_limit, residuals < lower_limit)
-        # Ensure that pixels not monitored are set to false
-        is_outlier[~is_monitored] = False
         # Update self.state
         if self.state is None:
             self.state = np.zeros_like(array, dtype=np.uint8)
-        # Left shift the pixels being monitored
-        self.state = np.left_shift(self.state, np.uint(1), where=is_monitored)
-        # Update state
+        # TODO: Pixels that could not be monitored (because cloud/Nan) should not be left_shifted
+        # see where argument of np.left_shift
+        self.state = np.left_shift(self.state, np.uint(1))
         self.state = self.state + is_outlier
-        # Locate confirmed breaks
-        flags = np.where(np.bitwise_and(self.state, self.bit_mask) == bit_mask,
-                         True, False)
-        flags[~is_monitored] = False
-        # Update mask
-        self.mask = np.where(flags, 3, mask)
-
-    @property
-    def bit_mask(self):
-        return sum([2**x for x in range(self.confirm_threshold)])
+        # TODO: update mask (confirmed breaks are no longer monitored)
 
     def _report(self):
         # TODO: add different levels of flagging (level 1, 2, 3, confirmed, ...)
         # TODO: Merge with updated mask to show unmonitored and previously disturbed areas
-        flags = np.where(np.bitwise_and(self.state, self.bit_mask) == bit_mask, 1, 0)
+        bit_mask = sum([2**x for x in range(self.confirm_threshold)])
+        flags = np.where(np.bitwise_and(self.state, bit_mask) == bit_mask, 1, 0)
         return flags.astype(np.uint8)
