7dc3d2019646465981f35091ade94bc0b795dae3
Loïc Dutrieux <loic.dutrieux@gmail.com>
2021-01-29 21:05:12 +0100

    Adapted brooks to new basecalss logic

--- nrt/brooks.py
+++ nrt/brooks.py
@@ -58,38 +58,18 @@ class Brooks(BaseNrt):
         # calculate the EWMA value for the end of the training period and save it
         self.process = self._init_process(residuals)
 
-    def monitor(self, array, date):
-        y_pred = self.predict(date)
-        residuals = array - y_pred
-        # Filtering of values with high threshold X-Bar and calculating new EWMA values
-        residuals[np.abs(residuals) > self.sensitivity * self.sigma] = np.nan
-        self.nodata = np.isnan(residuals)
-        self.process = self._update_process(array=residuals)
+    def _detect_extreme_ouliers(self, residuals, is_valid):
+        is_eoutlier = np.abs(residuals) > self.sensitivity * self.sigma
+        return np.logical_and(~is_eoutlier, is_valid)
 
-    def _report(self):
-        # signals severity of disturbance:
-        #    0 = not disturbed
-        #   >1 = disturbed (bigger number: higher severity)
-        #  255 = no data
-        # TODO no data signal works, but is ugly, maybe make it optional
-        signal = np.floor_divide(np.abs(self.process),
-                                 self.boundary).astype(np.uint8)
-        signal[self.nodata] = 255
-        return signal
-
-    @staticmethod
-    def _update_ewma(array, ewma, lambda_):
-        ewma_new = np.where(np.isnan(array),
-                            ewma,
-                            (1 - lambda_) * ewma + lambda_ * array)
-        return ewma_new
-
-    def _update_process(self, array):
+    def _update_process(self, residuals, is_valid):
         """Update process value (EWMA in this case) with new acquisition
 
         Args:
-            array (numpy.ndarray): 2 dimensional array corresponding to the residuals
+            residuals (numpy.ndarray): 2 dimensional array corresponding to the residuals
                 of a new acquisition
+            is_valid (np.ndarray): A boolean 2D array indicating where process
+                values should be updated
 
         Returns:
             numpy.ndarray: A 2 dimensional array containing the updated EWMA values
@@ -98,9 +78,21 @@ class Brooks(BaseNrt):
         if self.process is None:
             raise ValueError('Process has to be initialized before update')
         # Update ewma value for element of the input array that are not Nan
-        process_new = self._update_ewma(array=array, ewma=self.process,
+        process_new = self._update_ewma(array=residuals, ewma=self.process,
                                         lambda_=self.lambda_)
-        return process_new
+        self.process = np.where(is_valid, process_new, self.process)
+
+    def _detect_break(self):
+        is_break = np.floor_divide(np.abs(self.process),
+                                   self.boundary).astype(np.uint8)
+        return is_break
+
+    @staticmethod
+    def _update_ewma(array, ewma, lambda_):
+        ewma_new = np.where(np.isnan(array),
+                            ewma,
+                            (1 - lambda_) * ewma + lambda_ * array)
+        return ewma_new
 
     # TODO: only static methods can be jitted, make _update_ewma external to the class
     # and _init_process static
