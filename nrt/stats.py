006c8414fc59d4b0441e2cce40641638f2cb946c
Jonas Viehweger <jonas.viehweger@rwth-aachen.de>
2021-07-30 13:31:49 +0200

    Simplified weighted ols. Only accepts 1D time series without nan now

--- nrt/stats.py
+++ nrt/stats.py
@@ -43,58 +43,6 @@ def nanlstsq(X, y):
     return beta
 
 
-@numba.jit(nopython=True, cache=True)
-def weighted_nanlstsq(X, y):
-    """Return the weighted least-squares solution to a linear matrix equation
-
-    Analog to ``nanlstsq`` but X is unique for every observation in y
-
-    Args:
-        X ((K, M, N) np.ndarray): Weighted Matrix of independant variables,
-            unique for each timeseries K in y
-        y ((M, K) np.ndarray): Matrix of dependant variables
-
-    Returns:
-        np.ndarray: Least-squares solution for every y with unique X
-    """
-    beta = np.zeros((X.shape[2], y.shape[1]), dtype=np.float32)
-    for idx in range(y.shape[1]):
-        # Indexing one after the other, because Numba isn't supporting
-        # multiple advanced indices
-        y_ = y[:,idx]
-        X_ = X[idx]
-        isna = np.isnan(y_)
-        X_sub = X_[~isna]
-        y_sub = y_[~isna]
-
-        XTX = np.linalg.inv(np.dot(X_sub.T, X_sub))
-        XTY = np.dot(X_sub.T, y_sub)
-        beta[:,idx] = np.dot(XTX, XTY)
-    return beta
-
-
-@numba.jit(nopython=True, cache=True)
-def nanmedian_along_axis(arr, axis):
-    """Returns Mean along selected axis
-
-    Implementation to work with numba
-
-    Args:
-        arr (np.ndarray): N-Dimensional array
-        axis (int): Axis to calculate the median along
-
-    Returns:
-        np.ndarray: Median excluding nan along the axis
-
-    Reference:
-        http://en.wikipedia.org/wiki/Median_absolute_deviation
-    """
-    res = np.empty(arr.shape[axis], dtype=arr.dtype)
-    for idx in range(arr.shape[axis]):
-        arr_sub = arr[:, idx]
-        res[idx] = np.nanmedian(arr_sub)
-    return res
-
 
 @numba.jit(nopython=True, cache=True)
 def mad(resid, c=0.6745):
@@ -111,8 +59,7 @@ def mad(resid, c=0.6745):
         http://en.wikipedia.org/wiki/Median_absolute_deviation
     """
     # Return median absolute deviation adjusted sigma
-    return nanmedian_along_axis(
-        np.fabs(resid - nanmedian_along_axis(resid, axis=1)), axis=1) / c
+    return np.nanmedian(np.fabs(resid - np.nanmedian(resid))) / c
 
 # Weight scaling methods
 @numba.jit(nopython=True, cache=True)
