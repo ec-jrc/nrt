c91cb88cf5de14ea69d0e1f887d52b4a6a0654c8
Loïc Dutrieux <loic.dutrieux@gmail.com>
2022-03-18 19:23:47 +0100

    Copyright statement added in header of each source file

--- nrt/outliers.py
+++ nrt/outliers.py
@@ -16,6 +16,21 @@ Citations:
   Sensing of Environment 144 (March): 152–71.
   https://doi.org/10.1016/j.rse.2014.01.011.
 """
+# Copyright (C) 2022 European Union (Joint Research Centre)
+#
+# Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+# the European Commission - subsequent versions of the EUPL (the "Licence");
+# You may not use this work except in compliance with the Licence.
+# You may obtain a copy of the Licence at:
+#
+#   https://joinup.ec.europa.eu/software/page/eupl
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the Licence is distributed on an "AS IS" basis,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the Licence for the specific language governing permissions and
+# limitations under the Licence.
+
 import numpy as np
 
 from nrt.fit_methods import rirls, ols
